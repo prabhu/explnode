149b3eca915d1be79ae51e00538be921d5778117
Prabhu Subramanian <prabhu@shiftleft.io>
2020-11-05 11:52:43 +0000

    Better fix for proto

--- vulnerabilities/dep-lodash.js
+++ vulnerabilities/dep-lodash.js
@@ -6,19 +6,34 @@ const lodash = require('lodash');
 //if req.body.config == '{"constructor": {"prototype": {"isAdmin": true}}}' it will bypass the authentication
 function check(req, res) {
 
-    let config = {};
-    lodash.defaultsDeep(config, JSON.parse(req.body.config));
-
-    let user = getCurrentUser(config);
-    if(!user){
-      user = {};
+    let config = {
+      isAdmin: false
+    };
+    const userConfig = req.body.config;
+    // Validate userConfig here
+    if (!isValidUserConfig(userConfig)) {
+      res.send('Validation error');
     }
-
+    lodash.defaultsDeep(config, userConfig);
+    let user = getCurrentUser(config);
     if (user.isAdmin && user.isAdmin === true) {
         res.send('Welcome Admin')
     }else{
         res.send('Welcome User')
     }
+}
+
+function isValidUserConfig(userConfig) {
+  if (!userConfig) {
+    return false;
+  }
+  if (userConfig['constructor'] && typeof userConfig['constructor'] === 'function') {
+    return false;
+  }
+  if (userConfig['__proto__']) {
+    return false;
+  }
+  return true;
 }
 
 //fake function that get current user from session or db
