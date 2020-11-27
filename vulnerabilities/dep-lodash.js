551936ccc5090b312ad858c22a5877afc7c6927b
Prabhu Subramanian <prabhu@shiftleft.io>
2020-11-27 23:24:09 +0000

    prototype pollution

--- vulnerabilities/dep-lodash.js
+++ vulnerabilities/dep-lodash.js
@@ -26,7 +26,46 @@ function getCurrentUser(config){
   return config;
 }
 
+var merge = function(target, source) {
+    for(var attr in source) {
+        if(typeof(target[attr]) === "object" && typeof(source[attr]) === "object") {
+            merge(target[attr], source[attr]);
+        } else {
+            target[attr] = source[attr];
+        }
+    }
+    return target;
+};
+
+var pathAssignment = (obj, path, value) => {
+    var segments = path.split(".");
+    var key = segments.splice(0,1)[0];
+    if(segments.length) {
+        if(obj[key]) {
+            obj[key] = pathAssignment(obj[key], segments.join('.'), value);
+        } else {
+            obj[key] = pathAssignment({}, segments.join('.'), value);
+        }
+    } else {
+        obj[key] = value;
+    }
+    return obj;
+};
 
 router.post('/check-user',check)
+
+router.get('/example1/user/:userConfig',  (req,res) => {
+    let userConfig = req.params.userConfig;
+    let config = {}
+    merge(config, userConfig);
+    res.send("Config is" + config);
+});
+
+router.get('/example2/user/:userConfig/:userPath',  (req,res) => {
+    let userConfig = req.params.userConfig;
+    let config = {}
+    pathAssignment(config, req.params.userPath, userConfig);
+    res.send("Config is" + config);
+});
 
 module.exports = router
--- vulnerabilities/redos.js
+++ vulnerabilities/redos.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router()
 
 
-router.get("/tstMe", (req, res) => { 
+router.get("/tstMe", (req, res) => {
     var r = /([a-z]+)+$/;
 
     let match = r.test(req.params.id);
     res.send(match)
-    
+
 });
 
 
